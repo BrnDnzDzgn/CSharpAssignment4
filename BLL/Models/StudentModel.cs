ff248888489f551849c5b2346dca882791722967
bdduz <bdduz@GUNTER>
2024-10-05 07:53:50 -0700

    Add project files.

---
+++ BLL/Models/StudentModel.cs
@@ -0,0 +1,22 @@
+﻿using BLL.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable disable
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        public Student Record { get; set; } //has a relationship
+
+        public int Id => Record.Id;
+        public string Name => Record.Name;
+        public string Surname => Record.Surname;
+        public DateTime? BirthDate => Record.BirthDate;
+        public decimal? Gpa => Record.Gpa;
+
+    }
+}
